27973172a66bb0a33b2b0e6df42b05f3052f8eff
Chris <craveend@umich.edu>
2016-12-13 16:07:19 -0500

    forward frequency bug fix

--- forward_frequency.py
+++ forward_frequency.py
@@ -5,14 +5,14 @@ import nltk.tokenize
 from sklearn import preprocessing
 import hashlib
 
-#takes in dictionary of keywords w/ frequencies as values & text file as a string
-def forward_freq_calc(dictionary, textfile):
+#takes in keyword_map of keywords w/ frequencies as values & text file as a string
+def forward_freq_calc(keyword_map, textfile):
 	t = open(textfile, "r")
 	line = t.readline()
 
 	ffreq_dict = {}
 	paragraphs = 0
-	occurred = false
+	occurred = False
 
 	#grab total # of paragraphs within textfile
 	while line:
@@ -22,42 +22,57 @@ def forward_freq_calc(dictionary, textfile):
 		paragraphs+=1
 		line = t.readline()
 
-	t = open(textfile, "r")
-	line = t.readline()
-
 
 	#for each key, go through entire textfile and calculate forward frequency
-	for key in dictionary.iteritems():
+	for key, value in keyword_map.iteritems():
 		freq = 0
 		ffreq = 0
+
+		t = open(textfile, "r")
+		line = t.readline()
+
+
 		while line:
 			#for each class chunk
 			while line.strip() != '':
 				line = line.strip()
-				if line == "exit": break
-				sentence = nltk.tokenize.word_tokenize(line)
 				
-				for word in sentence:
-					if key == word:
-						#increase frequency once per paragraph if word is found
-						if occurred == false:
-							freq+=1
-							occurred = true
+				#for word in sentence:
+				if line.find(key) != -1:
+					#increase frequency once per paragraph if word is found
+					if occurred == False:
+						freq+=1
+						occurred = True
 
 				line = t.readline()
 
 			#at end of a paragraph
-			occurred = false
+			occurred = False
 
 			#get rid of blank line
 			line = t.readline()
 
 		#after going through entire doc, calculate forward frequency and store
-		ffreq = float(dictionary[key])*float(freq)/float(paragraphs)
+		ffreq = float(keyword_map[key])*float(freq)/float(paragraphs)
 
-		#add keyword candidate to dictionary with value of its forward frequency
+		#add keyword candidate to keyword_map with value of its forward frequency
 		ffreq_dict[key] = ffreq
+
+
 
 	return ffreq_dict
 
+keyword_map = {
+  'Present': 1, 'Scalable Distributed Information Management System (SDIMS)': 1, 'aggregates information': 1,
+  'large-scale networked systems': 1, 'large-scale distributed applications': 1,
+  'information': 1, 'summary': 1, 'global': 1, 'information': 1
+}
+
+#dict = {}
+#dict = forward_freq_calc(keyword_map, "testdoc.txt")
+
+
+#for word in dict:
+#	print word
+#	print dict[word]
 
